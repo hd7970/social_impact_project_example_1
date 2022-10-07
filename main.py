a907484b7d92a8131505f45679f27e6b8621141a
will <William.Harvey@gordon.edu>
2022-10-07 16:59:11 -0400

    - reformatted cli - comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def variables_demo():
+def variable_type_demo():
     i = 10  # integer
     f = 1.1  # floating point
     c = 'H'  # character (python makes it a string)
@@ -32,19 +32,35 @@ def type_change_demo():
     print("x = ", x, type(x))
 
 
+# not needed for video
+def variables():
+    print("1. variable types")
+    print("2. type changes")
+    print("/. exit")
+    while True:
+        user_in = input("please make a selection (1-9, /) : ")
+        if user_in == '1':
+            variable_type_demo()
+        elif user_in == '2':
+            type_change_demo()
+        elif user_in == '/':
+            exit(0)
+        else:
+            exit(1)
 
+# This does not need to be shown in the video.  This is more so for making sure my code works, and is usable.
 def main():
-    print("A. variable demo")
+    print("A. variable demos")
     print("/. exit")
     while True:
         user_in = input("please make a selection (A-Z, /) : ")
         if user_in == 'A' or 'a':
-            variables_demo()
-        elif user_in == 0:
+            variables()
+        elif user_in == '/':
             exit(0)
         else:
             exit(1)
 
 
-
+# This single function call is what allows the program to run.
 main()
