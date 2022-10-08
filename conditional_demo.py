d9a439eeb7770857133672616d66eac5f126ba11
will <William.Harvey@gordon.edu>
2022-10-08 10:18:51 -0400

    - added conditional_demo.py

---
+++ conditional_demo.py
@@ -0,0 +1,19 @@
+def conditionals():
+    x = 7
+    y = 11
+
+    # basic if-else
+    if x == y:
+        print("yep they sure are equal")
+    else:
+        print("in what world are those equal")
+
+    # elif: else if
+    if x > 7:
+        print("7 has nothing on this number")
+    elif x > 8:
+        print("seven eight nine")
+    elif x < 7:
+        print("well it's certainly less than 7")
+    else:
+        print("this is most definitely a 7")
